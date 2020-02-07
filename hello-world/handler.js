bb1c0e2b42b544a19adf01856e46063984e07f6d
Michael Wright <wrigh517@gmail.com>
2020-02-07 00:01:33 +0000

    Runs hello-world

---
+++ hello-world/handler.js
@@ -0,0 +1,6 @@
+// Event: an object that contains all request data
+// context: object containing AWS-specific values (e.g. request ID, log group name, etc.)
+// callback: function called with either error response as first arg or success response as second arg
+module.exports.run = (event, context, callback) => {
+    callback(null, "Hello World")
+}
